35899a1442beb321d344c33a18114f7ca40f31b9
Alex Licano <licano95@gmail.com>
2015-02-05 21:24:17 -0700

    first commit to the new_options app and updated README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewOptions::Application.config.secret_key_base = '1dce5725fbf47d38b3f1de1d20e79551a73878f6314769f621ef865bd9ed672db38b9f386ecae9782eb93e3d5b5f9de548b86b8ab58c1cd77bfc38a35790f5fb'
